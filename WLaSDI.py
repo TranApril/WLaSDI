183cb7940edc5fca8b3607bf21b9219617c915af
April Tran <102925411+TranApril@users.noreply.github.com>
2024-11-20 11:15:30 -0700

    Update WLaSDI.py

--- WLaSDI.py
+++ WLaSDI.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.linalg as LA
-import WLaSDI.wsindy as ws
+import wsindy as ws
+#import LaSDI.pywsindy as ws
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp2d, Rbf
 import scipy.integrate as integrate
@@ -24,7 +25,6 @@ class WLaSDI:
        nearest_neigh: Number of nearest neigh in Local DI
        Coef_interp_method: Either interp2d or Rbf method for coefficient interpolation.
     """
-    
     def __init__(self, encoder, decoder, NN = False, device = 'cpu', Local = False, Coef_interp = False, nearest_neigh = 4, Coef_interp_method = None, plot_fname = 'latent_space_dynamics.png'):
         
         self.Local = Local
@@ -51,7 +51,7 @@ class WLaSDI:
             
         return
     
-    def train_dynamics(self, ls_trajs, training_values, dt, normal = 1, degree = 1, include_interaction=False, LS_vis = True, gamma = 0, threshold = 0, L = 30, overlap = 0.5, opt_tfsupp = False, sampling_rate = 1):
+    def train_dynamics(self, ls_trajs, training_values, t, normal = 1, degree = 1, LS_vis = True, gamma = 0, threshold = 0, L = 30, overlap = 0.5, useGLS = 0):
         """
         Approximates the dynamical system for the latent-space. Local == True, use generate_FOM. 
         
@@ -65,37 +65,36 @@ class WLaSDI:
            WSINDy parameters
            L: test function support
            overlap: how much 2 consecutive test functions overlap. 
-           opt_tfsupp: toggle the use of optimal test function support
+           gamma: regularization param
+           threshold: sparsification param (always set to 0)
+
         """
-        self.dt = dt
-        self.LS_vis = LS_vis
+        self.training_values = training_values
+        self.ls_trajs = ls_trajs
+        self.t = t
         self.normal = normal
         self.degree = degree
+        self.LS_vis = LS_vis
+        self.gamma = gamma
         self.threshold = threshold
-        self.opt_tfsupp = opt_tfsupp
         self.L = L
         self.overlap = overlap
-        self.gamma = gamma
+        self.useGLS = useGLS
+        
         data_LS = []
-        for traj in ls_trajs:
-            data_LS.append(traj[0: len(traj)-1: sampling_rate]/normal)
-            
-        #print(data_LS[0].shape)
+        for traj in self.ls_trajs:
+            data_LS.append(traj/self.normal)
+        self.data_LS = data_LS
+
+    
     
         if self.Local == False:
-            model = ws.wsindy(polys=np.arange(0, degree+1), multiple_tracjectories=True, ld = threshold, gamma = gamma)
-
-            #generating t
-            t = [np.linspace(0, sampling_rate*dt*(len(data_LS[-1])-1), int(len(data_LS[-1])))]
-            #print(t[0].shape)
-            #print(t[0])
+            model = ws.wsindy(polys=np.arange(0, degree+1), multiple_tracjectories=True, ld = threshold, gamma = gamma, useGLS = self.useGLS)
+            time = [self.t]
             for i in range(1, len(data_LS)):
-                t.append(np.linspace(0, sampling_rate*dt*(len(data_LS[-1])-1), int(len(data_LS[-1]))))
+                time.append(self.t)
 
-            #using uniform grid
-            model.getWSindyUniform(data_LS, t, L = L, overlap=overlap, opt_tfsupp=opt_tfsupp)
-
-
+            model.getWSindyUniform(data_LS, time, L = L, overlap=overlap)
             self.model = model
             if LS_vis == True:
                 if self.NN == True:
@@ -104,52 +103,36 @@ class WLaSDI:
                 else:
                     DcTech = 'LaSDI-LS Latent-Space Visualization'
                     DcTech = 'Latent-Space Dynamics by Linear Compression'
-                time = np.linspace(0, dt*(len(data_LS[-1])-1), len(data_LS[-1]))
                 fig = plt.figure()
                 fig.set_size_inches(9,6)
-                ax = plt.axes()
-                ax.set_title(DcTech)
+                #ax = plt.axes()
+                #ax.set_title(DcTech)
                 labels = {'orig': 'Latent-Space Trajectory', 'new': 'Approximated Dynamics'}
                 for dim in range(data_LS[-1].shape[1]):
-                    plt.plot(time[:-1], data_LS[-1][:-1,dim], alpha = .5, label = labels['orig'])
+                    plt.plot(t[:-1], data_LS[-1][:-1,dim], alpha = .5, label = labels['orig'])
                     labels['orig'] = '_nolegend_'
                 plt.gca().set_prop_cycle(None)
 
-                new = model.simulate(x0 = data_LS[-1][0], t_span = np.array([0, dt*len(data_LS[-1])]), t_eval = np.linspace(0, dt*(len(data_LS[-1])-1), len(data_LS[-1])))
-                #print(data_LS[-1][0])
-                #print(np.array([0, dt*len(data_LS[-1])]))
-                #print(np.linspace(0, dt*(len(data_LS[-1])-1), len(data_LS[-1])).shape)
-                #print(new.shape)
-                #print(model.coef)
-
+                new = model.simulate(x0 = data_LS[-1][0], t_span = np.array([t[0], t[-1]]), t_eval = self.t)
                 for dim in range(data_LS[-1].shape[1]):
-                    plt.plot(time, new[:,dim], '--', label = labels['new'])
+                    plt.plot(t, new[:,dim], '--', label = labels['new'])
                     labels['new'] = '_nolegend_'
-                ax.legend()
-                ax.set_xlabel('Time')
-                ax.set_ylabel('Magnitude')
-                #plt.savefig(self.plot_fname)
+                plt.legend()
+                plt.xlabel('Time')
+                plt.ylabel('Magnitude')
+                plt.tight_layout()
+                #plt.savefig('figures/resimu', transparent = True)
             return model.coef 
         elif self.Coef_interp == True:
-            #("Local approach WITH SINDy coefficient interpolation")
+            #print("Local approach WITH SINDy coefficient interpolation")
             if self.Coef_interp_method == None:
                 print('WARNING: Please specify an interpolation method either interp2d or Rbf')
-            self.model_list = []
-            self.training_values = training_values
-            self.dt = dt
-            self.degree = degree
-            self.length = len(data_LS[0])
+            model_list = []
+           
             for i, _ in enumerate(training_values):
-                model = ws.wsindy(polys=np.arange(0, self.degree+1), multiple_tracjectories=False, ld=threshold, gamma=gamma )
-                #generating t
-                t = np.linspace(0, dt*(len(data_LS[i])-1), len(data_LS[i]))
-
-                #using Adaptive grid
-                #model.getWsindyAdaptive(data_LS[i], t)
-
-                #using uniform grid
-                model.getWSindyUniform(data_LS[i], t, opt_tfsupp=opt_tfsupp, L = L, overlap=overlap)
-                self.model_list.append(model.coef)
+                model = ws.wsindy(polys=np.arange(0, self.degree+1), multiple_tracjectories=True, ld=threshold, gamma=gamma, useGLS = self.useGLS)
+                model.getWSindyUniform([data_LS[i]], [t], L = L, overlap=overlap)
+                model_list.append(model.coef)
                 self.tags = model.tags
                 if LS_vis == True:
                     if self.NN == True:
@@ -158,41 +141,33 @@ class WLaSDI:
                     else:
                         DcTech = 'LaSDI-LS Latent-Space Visualization'
                         DcTech = 'Latent-Space Dynamics by Linear Compression'
-                    time = np.linspace(0, dt*(len(data_LS[-1])-1), len(data_LS[-1]))
                     fig = plt.figure()
                     fig.set_size_inches(9,6)
                     ax = plt.axes()
                     ax.set_title(DcTech)
                     labels = {'orig': 'Latent-Space Trajectory', 'new': 'Approximated Dynamics'}
                     for dim in range(data_LS[-1].shape[1]):
-                        plt.plot(time[:-1], data_LS[-1][:-1,dim], alpha = .5, label = labels['orig'])
+                        plt.plot(t[:-1], data_LS[i][:-1,dim], alpha = .5, label = labels['orig'])
                         labels['orig'] = '_nolegend_'
                     plt.gca().set_prop_cycle(None)
 
-                    new = model.simulate(data_LS[-1][0], np.array([0, dt*len(data_LS[-1])]), np.linspace(0, dt*(len(data_LS[-1])-1), len(data_LS[-1])))
+                    new = model.simulate(data_LS[i][0], t_span = np.array([t[0], t[-1]]), t_eval = self.t)
                     for dim in range(data_LS[-1].shape[1]):
-                        plt.plot(time, new[:,dim], '--', label = labels['new'])
+                        plt.plot(t, new[:,dim], '--', label = labels['new'])
                         labels['new'] = '_nolegend_'
                     ax.legend()
                     ax.set_xlabel('Time')
                     ax.set_ylabel('Magnitude')
                     #plt.savefig(self.plot_fname)
-
+            self.model_list = model_list
             return self.model_list
         else:
-            #print("Local approach WITHOUT SINDy coefficient interpolation")
-            self.ls_trajs = ls_trajs
-            self.training_values = training_values
-            self.dt = dt
-            self.degree = degree
-            self.include_interaction = include_interaction
-            self.data_LS = data_LS
-            
+            print("Local approach WITHOUT SINDy coefficient interpolation")
             return 
         
     
         
-    def generate_ROM(self,pred_IC,pred_value,t):
+    def generate_ROM(self,pred_IC,pred_value,t, epsilon = 1, function='gaussian'):
         """
         Takes initial condition in full-space and associated parameter values and generates forward in time using the trained dynamics from above.
         Inputs:
@@ -202,8 +177,8 @@ class WLaSDI:
         """
         IC = self.IC_gen(pred_IC)
         if self.Local == False: # Global approach
-            latent_space_recon = self.normal*self.model.simulate(IC/self.normal, np.array([t[0], t[-1]]), t)
-            FOM_recon = self.decoder(latent_space_recon)
+            self.latent_space_recon = self.normal*self.model.simulate(IC/self.normal, np.array([t[0], t[-1]]), t)
+            FOM_recon = self.decoder(self.latent_space_recon)
             if self.NN == False:
                 return FOM_recon.T
             else:
@@ -218,25 +193,24 @@ class WLaSDI:
             dist_index = np.argsort(dist)[0:k]
             self.dist_index = dist_index
 
-
             if self.Coef_interp == False: # WITHOUT SINDy coefficient interpolation
                 local = []
                 for iii in dist_index:
                     local.append(self.data_LS[iii])
-                model = ws.wsindy(polys=np.arange(0, self.degree+1), multiple_tracjectories=True, ld=self.threshold, gamma=self.gamma)
-                t_val = [np.linspace(0, self.dt*(len(local[-1])-1), len(local[-1]))]
+                model = ws.wsindy(polys=np.arange(0, self.degree+1), multiple_tracjectories=True, ld=self.threshold, gamma=self.gamma, useGLS = self.useGLS)
+                t_val = [self.t]
                 for i in range(1, len(local)):
-                    t_val.append(np.linspace(0, self.dt*(len(local[-1])-1), len(local[-1])))
+                    t_val.append(self.t)
 
                 #using uniform grid
-                model.getWSindyUniform(local, t_val, opt_tfsupp = self.opt_tfsupp, L = self.L, overlap = self.overlap )
+                model.getWSindyUniform(local, t_val, L = self.L, overlap = self.overlap )
 
                 #using adaptive grid
-                #model.getWsindyAdaptive(local, t_val)
+                #model.getWsindyAdaptive(local, t_val, K = 200)
 
                 self.training_time = time.time()-training_time_start
-                latent_space_recon = self.normal*model.simulate(IC/self.normal, np.array([t[0], t[-1]]), t)
-                FOM_recon = self.decoder(latent_space_recon)
+                self.latent_space_recon = self.normal*model.simulate(IC/self.normal, np.array([t[0], t[-1]]), t)
+                FOM_recon = self.decoder(self.latent_space_recon)
                 
                 if self.LS_vis == True:
                     if self.NN == True:
@@ -245,26 +219,20 @@ class WLaSDI:
                     else:
                         DcTech = 'LaSDI-LS Latent-Space Visualization'
                         DcTech = 'Latent-Space Dynamics by Linear Compression'
-                    ti = np.linspace(0, self.dt*(len(local[-1])-1), len(local[-1]))
+                    #ti = np.linspace(0, self.dt*(len(local[-1])-1), len(local[-1]))
                     fig = plt.figure()
                     fig.set_size_inches(9,6)
                     ax = plt.axes()
                     ax.set_title(DcTech)
                     labels = {'orig': 'Latent-Space Trajectory', 'new': 'Approximated Dynamics'}
                     for dim in range(local[-1].shape[1]):
-                        plt.plot(ti[:-1], local[-1][:-1,dim], alpha = .5, label = labels['orig'])
+                        plt.plot(t[:-1], local[-1][:-1,dim], alpha = .5, label = labels['orig'])
                         labels['orig'] = '_nolegend_'
                     plt.gca().set_prop_cycle(None)
 
-                    new = model.simulate(x0 = local[-1][0], t_span = np.array([0, self.dt*len(local[-1])]), t_eval = np.linspace(0, self.dt*(len(local[-1])-1), len(local[-1])))
-                    #print(data_LS[-1][0])
-                    #print(np.array([0, dt*len(data_LS[-1])]))
-                    #print(np.linspace(0, dt*(len(data_LS[-1])-1), len(data_LS[-1])).shape)
-                    #print(new.shape)
-                    #print(model.coef)
-
+                    new = model.simulate(x0 = local[-1][0], t_span = np.array([t[0], t[-1]]), t_eval = t)
                     for dim in range(local[-1].shape[1]):
-                        plt.plot(ti, new[:,dim], '--', label = labels['new'])
+                        plt.plot(t, new[:,dim], '--', label = labels['new'])
                         labels['new'] = '_nolegend_'
                     ax.legend()
                     ax.set_xlabel('Time')
@@ -274,24 +242,18 @@ class WLaSDI:
                     return FOM_recon.T
                 else:
                     return FOM_recon
-
             else: # WITH SINDy coefficient interpolation
-                # print(self.dist_index)
                 self.coeff_interp_model = np.empty(self.model_list[0].shape)
                 self.training_time = 0
-                
                 # Compute SINDy coefficients of the testing parameter by interpolation
                 for ls_dim in range(self.model_list[0].shape[0]):
                     for func_index in range(self.model_list[0].shape[1]):
                         f = self.Coef_interp_method(self.training_values[dist_index,0], 
                                                     self.training_values[dist_index,1], 
-                                                    np.array(self.model_list)[dist_index,ls_dim,func_index])
+                                                    np.array(self.model_list)[dist_index,ls_dim,func_index], function=function, epsilon = epsilon)
 
                         self.coeff_interp_model[ls_dim, func_index] = f(pred_value[0], pred_value[1])
-                #print(self.coeff_interp_model) # (ns,nl)
-            
-                self.time = np.arange(0,self.dt*self.length, self.dt)
-                #print(self.tags)
+        
                 def simulate(x0, t_span, t_eval, coef):
                     rows, cols = self.tags.shape
                     tol_ode = 10**(-13)
@@ -303,10 +265,7 @@ class WLaSDI:
                         return term.dot(coef)
                     sol = solve_ivp(fun = rhs, t_eval=t_eval, t_span=t_span, y0=x0, rtol=tol_ode)
                     return sol.y.T
-                self.latent_space_recon = self.normal*simulate(IC/self.normal, t_eval=self.time, t_span = np.array([self.time[0], self.time[-1]]), coef = self.coeff_interp_model)
-                
-                #plt.plot(self.latent_space_recon)
-                # high-dimensional dynamics
+                self.latent_space_recon = self.normal*simulate(IC/self.normal, t_eval=t, t_span = np.array([t[0], t[-1]]), coef = self.coeff_interp_model)
                 FOM_recon = self.decoder(self.latent_space_recon)
                 if self.NN == False:
                     return FOM_recon.T
